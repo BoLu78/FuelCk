bd9714066fd00323ef14485ea7a4e7b375c5c228
BoLu78 <126019350+BoLu78@users.noreply.github.com>
2026-04-06 09:01:28 -0400

    6.7

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "6.6";
+const APP_VERSION = "6.7";
 const LBS_TO_KG = 0.45359237;
 const US_GALLON_TO_LITERS = 3.785411784;
 const INVALID_ALERT_MESSAGE = "Invalid data: required uplift must be positive";
--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,13 @@
-const CACHE_NAME = "rampcheck-v6.6";
+const CACHE_NAME = "rampcheck-v6.7";
 const OFFLINE_FALLBACK_URL = "./index.html";
 const APP_FILES = [
   "./",
-  "./?v=6.6",
+  "./?v=6.7",
   "./index.html",
   "./app.js",
-  "./app.js?v=6.6",
+  "./app.js?v=6.7",
   "./manifest.json",
-  "./manifest.json?v=6.6",
+  "./manifest.json?v=6.7",
   "./service-worker.js",
   "./assets/tripinfo-logo-neos.png",
   "./icons/icon-192.png",
